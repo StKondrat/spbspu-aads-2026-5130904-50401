be6809b2d0f3807a1a6082ece1954b13a652e061
StKondrat <kndrt13@icloud.com>
2026-05-11 16:35:40 +0300

    edit main.cpp

--- kondrat.stepan/S3/main.cpp
+++ kondrat.stepan/S3/main.cpp
@@ -1,2 +1,82 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <string>
+#include "graphs-table.hpp"
+#include "hash-table.hpp"
+#include "hash-func.hpp"
+
+int main(int argc, char ** argv)
+{
+  using namespace kondrat;
+
+  if (argc != 2)
+  {
+    std::cerr << "invalid arguments\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file)
+  {
+    std::cerr << "cannot open file\n";
+    return 1;
+  }
+
+  GraphsTable table;
+
+  try
+  {
+    table.readFile(file);
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+
+  using cmd_t = void (GraphsTable::*)(std::istream &, std::ostream &, std::string);
+
+  HashTable< std::string, cmd_t, blake2, Equal< std::string > > commands;
+
+  commands.add("graphs", &GraphsTable::graphs);
+  commands.add("vertexes", &GraphsTable::vertexes);
+  commands.add("outbound", &GraphsTable::outbound);
+  commands.add("inbound", &GraphsTable::inbound);
+  commands.add("bind", &GraphsTable::bind);
+  commands.add("cut", &GraphsTable::cut);
+  commands.add("create", &GraphsTable::create);
+  commands.add("merge", &GraphsTable::merge);
+  commands.add("extract", &GraphsTable::extract);
+
+  std::string cmd;
+
+  while (std::cin >> cmd)
+  {
+    std::string graphName;
+
+    if (cmd != "graphs")
+    {
+      if (!(std::cin >> graphName))
+      {
+        std::cout << "<INVALID COMMAND>\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+        continue;
+      }
+    }
+
+    try
+    {
+      cmd_t command = commands.get(cmd);
+      (table.*command)(std::cin, std::cout, graphName);
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+}
