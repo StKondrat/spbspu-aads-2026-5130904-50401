01ecd77a18b76a028f1a8992ce8963254a1cca1c
StKondrat <kndrt13@icloud.com>
2026-05-22 00:04:42 +0300

    add bst-node.hpp

---
+++ kondrat.stepan/S4/bst-node.hpp
@@ -0,0 +1,36 @@
+#ifndef BST_NODE_HPP
+#define BST_NODE_HPP
+
+#include <utility>
+
+namespace kondrat
+{
+  template< class Key, class Value >
+  class Node
+  {
+  public:
+    Node(const Key & key, const Value & value);
+  private:
+    template< class K, class V >
+    friend class BSTIterator;
+    template< class K, class V >
+    friend class CBSTIterator;
+    template< class K, class V, class C >
+    friend class BSTree;
+
+    std::pair< Key, Value > data_;
+    Node * right_;
+    Node * left_;
+    Node * parent_;
+  };
+}
+
+template< class Key, class Value >
+kondrat::Node< Key, Value >::Node(const Key & key, const Value & value):
+data_(std::pair< Key, Value >(key, value)),
+right_(nullptr),
+left_(nullptr),
+parent_(nullptr)
+{}
+
+#endif
