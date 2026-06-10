e5830f2a8bf49401516e7adddead869295cc5c35
StKondrat <kndrt13@icloud.com>
2026-06-11 00:31:21 +0300

    some fixes after review

--- kondrat.stepan/S2/test-funcs.cpp
+++ kondrat.stepan/S2/test-funcs.cpp
@@ -29,84 +29,11 @@ BOOST_AUTO_TEST_CASE(BinaryOperations)
   BOOST_CHECK(applyUnaryOperation(num2, "#") == 1);
 }
 
-BOOST_AUTO_TEST_CASE(GetOperatorType)
+BOOST_AUTO_TEST_CASE(CalculateExpression)
 {
-  BOOST_CHECK(getOperatorType("1") == 0);
-  BOOST_CHECK(getOperatorType("+") == 1);
-  BOOST_CHECK(getOperatorType("-") == 1);
-  BOOST_CHECK(getOperatorType("*") == 1);
-  BOOST_CHECK(getOperatorType("/") == 1);
-  BOOST_CHECK(getOperatorType("%") == 1);
-  BOOST_CHECK(getOperatorType("#") == 2);
-}
-
-BOOST_AUTO_TEST_CASE(IsOperator)
-{
-  BOOST_CHECK(isOperator("1") == false);
-  BOOST_CHECK(isOperator("+") == true);
-  BOOST_CHECK(isOperator("-") == true);
-  BOOST_CHECK(isOperator("*") == true);
-  BOOST_CHECK(isOperator("/") == true);
-  BOOST_CHECK(isOperator("%") == true);
-  BOOST_CHECK(isOperator("#") == true);
-}
-
-BOOST_AUTO_TEST_CASE(GetPriority)
-{
-  BOOST_CHECK(getPriority("#") == 3);
-  BOOST_CHECK(getPriority("*") == 2);
-  BOOST_CHECK(getPriority("/") == 2);
-  BOOST_CHECK(getPriority("%") == 2);
-  BOOST_CHECK(getPriority("+") == 1);
-  BOOST_CHECK(getPriority("-") == 1);
-  BOOST_CHECK(getPriority("1") == 0);
-}
-
-BOOST_AUTO_TEST_CASE(Tokenize)
-{
-  Queue< std::string > res = tokenize("2 + 2 * 33 + # 12");
-
-  BOOST_CHECK(res.size() == 8);
-  BOOST_CHECK(res.first() == "2");
-  BOOST_CHECK(res.last() == "12");
-}
-
-BOOST_AUTO_TEST_CASE(Postfix)
-{
-  Queue< std::string > res = tokenize("2 + 2 * 33 + # 12");
-
-  res = infixToPostfix(res);
-
-  BOOST_CHECK(res.size() == 8);
-  BOOST_CHECK(res.first() == "2");
-  BOOST_CHECK(res.last() == "+");
-
-  res.drop();
-  res.drop();
-
-  BOOST_CHECK(res.drop() == "33");
-  BOOST_CHECK(res.drop() == "*");
-  BOOST_CHECK(res.drop() == "+");
-}
-
-BOOST_AUTO_TEST_CASE(ReadEvaluatePostfixPrint)
-{
-  Stack< ll > res;
-
-  std::stringstream in1("2 + 2 * 2 * # 10 * ( 5 + 1 )");
-  std::stringstream in2("2 + 2 * 2");
-  readExpressions(in1, res);
-
-  BOOST_CHECK(res.size() == 1);
-  BOOST_CHECK(res.first() == 26);
-
-  readExpressions(in2, res);
-
-  std::ostringstream out;
-  printResults(res, out);
-
-  std::string done = out.str();
-  BOOST_CHECK(done == "6 26\n");
+  BOOST_CHECK(calculateExpression("2 + 2 * 2") == 6);
+  BOOST_CHECK(calculateExpression("2 + 2 * 2 * # 10 * ( 5 + 1 )") == 26);
+  BOOST_CHECK(calculateExpression("10 % 4") == 2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
